21e11dcd453fce4a6edc99581676bd995b494e99
jonn <joliversalamanca@gmail.com>
2025-09-25 09:01:47 -0400

    se creo el componente TicketLayout

---
+++ database/seeders/PermissionSedder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSedder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions  = [
+            'manage_tickets',
+            'access_admin_panel',
+            'manage_users',
+            'manage_roles',
+            'view_reports',
+        ];    
+        
+        foreach ($permissions as $pemissioin) {
+            Permission::create([
+                'name' => $pemissioin
+                ]);
+        }
+    }
+}
--- routes/control-vehicular.php
+++ routes/control-vehicular.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return "control vehicular";
+    return view('control-vehicular.ticket');
 });
